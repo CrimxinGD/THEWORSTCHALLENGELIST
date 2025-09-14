832d9f16568e1e8caee281ab5f2396b120b07a4a
CrimxinGD <michaelsheppard584@gmail.com>
2025-09-14 17:39:31 -0230

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,7 +11,7 @@ const scale = 5;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 400) {
+    if (rank > 250) {
         return 0;
     }
     if (rank > 75 && percent < 100) {
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-3*Math.pow(rank-1, 3) + 140) *
+    let score = (-1*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
