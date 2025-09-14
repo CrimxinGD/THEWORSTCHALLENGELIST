fde9e29e48046f5d34d747d533e9b52e27231390
CrimxinGD <michaelsheppard584@gmail.com>
2025-09-14 17:37:55 -0230

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-30*Math.pow(rank-1, 3) + 350) *
+    let score = (-3*Math.pow(rank-1, 3) + 140) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
